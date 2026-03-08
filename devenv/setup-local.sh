fb00260dff30c6b6ffc68d1bb598d01a2d28fa22
Código Uranio <111033640+codigouranio@users.noreply.github.com>
2026-03-08 21:21:58 +0000

    wip

--- devenv/setup-local.sh
+++ devenv/setup-local.sh
@@ -1,2 +1,10 @@
 (docker compose down --rmi local && docker compose up --build)
+
 (cd ../.. && ./apps/api-service && npx prisma migrate reset --force)
+
+# start frontend 
+
+cd ./apps/my-resume && yarn install && yarn dev
+
+# start backend
+cd ./apps/api-resume && yarn install && yarn start
